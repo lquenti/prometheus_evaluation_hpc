5ef7a5e7e3f45ca2239524941dbfd596d96f704d
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-11-14 14:45:21 +0100

    run script for node_exporter

---
+++ v1/node_exporter/node_exporter.sh
@@ -0,0 +1,7 @@
+./node_exporter --collector.disable-defaults \
+  --collector.cpu \
+  --collector.cpufreq \
+  --collector.infiniband \
+  --collector.meminfo \
+  --collector.netdev \
+  --collector.vmstat
