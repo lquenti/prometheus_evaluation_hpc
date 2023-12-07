4cf0199d4b007c2ce6c866690dc38e43c72f0860
Lars Quentin <lars.quentin@stud.uni-goettingen.de>
2023-12-07 10:19:19 +0100

    have random metrics

--- v1/prometheus/api.py
+++ v1/prometheus/api.py
@@ -1,12 +1,32 @@
 import os
-import sys
+import random
 import threading
 
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, Response
 
+# Change to configure
+NUMBER_OF_METRICS=50
+INT_RANGE = (0, 2**32)
+
 counter = 0
 counter_lock = threading.Lock()
+
+# We do not want to rely on `random` for random numbers because it uses
+# the current milliseconds as a seed. Since we are spawning that many processes
+# parallely I would rather have a more independent seed.
+#
+# I thought about using `/dev/urandom` (through `os.urandom()`) but both the OS call
+# and the byte-to-number-in-python conversion is too costly
+#
+# Thus, we do some middle-ground:
+# - Get 8 random bits from os.urandom(), interpret as unsigned byte
+# - Do that amount of warmup with mersenne random
+# - use mersenne for following rng
+warmup_iterations = int.from_bytes(os.urandom(1), byteorder='big')
+rng = random.Random()
+for _ in range(warmup_iterations):
+    rng.random()
 
 def inc():
     global counter
@@ -37,15 +57,13 @@ async def lifespan(_app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-
 @app.get("/metrics")
 def get_metrics(response: Response):
     inc()
 
-    # TODO make dynamic
     metrics = [
-        "some_key 1337",
-        "another_key 42",
+        f"keyname{i} {rng.randint(*INT_RANGE)}"
+        for i in range(1, NUMBER_OF_METRICS+1)
     ]
 
     metrics_data = "\n".join(metrics)
